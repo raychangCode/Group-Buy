03b5f8704202e6a997d79bc7c56723d466c4b535
Ken Wu <kenwu1009us@gmail.com>
2022-04-03 11:45:44 -0500

    upload header.js

---
+++ client/src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="https://img.onl/I6oyHp"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Header;
