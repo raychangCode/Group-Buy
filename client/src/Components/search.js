1fa38a5af37ccbe89cbba40c8cfe8ebd65979a3b
Ken Wu <kenwu1009us@gmail.com>
2022-04-05 00:00:21 -0500

    fix search.js

--- client/src/Components/search.js
+++ client/src/Components/search.js
@@ -3,18 +3,25 @@ import Axios from 'axios';
 import './search.css';
 
 function SearchPost() {
+  const [expirationDate, setExpirationDate] = useState("");
+  const [groupLimit, setGroupLimit] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState("");
   const [productName, setProductName] = useState("");
+  const [storeName, setStoreName] = useState("");
+  const [price, setPrice] = useState("");
+  const [link, setLink] = useState("");
   const [searchPostList, setSearchPostList] = useState([]);
-  const [userName, setPostId] = useState("");
-  const [userId, setUserId] = useState("");
-  const [expirationDate, setexpirationDate] = useState("");
-  const [numOfPost, setnumOfPost] = useState("");
-  const [categoryId, setcategoryId] = useState("");
-  const [averageGroupLimit, setaverageGroupLimit] = useState("");
+
 
   const searchPost = async () => {
     const response = await Axios.post("http://localhost:3001/post/search/?s=" + productName, {
+      expirationDate: expirationDate,
+      groupLimit: groupLimit,
+      paymentMethod: paymentMethod,
       productName: productName,
+      storeName: storeName,
+      price: price,
+      link: link
     });
     setSearchPostList(response.data)
   };
@@ -35,7 +42,13 @@ function SearchPost() {
         {searchPostList.map((val) => {
           return (
             <div className="card">
-              <h1>Product name: {val.productName}</h1>
+              <h5>Product name: {val.productName}</h5>
+              <h5>Store Name: {val.storeName}</h5>
+              <h5>Price: {val.price}</h5>
+              <h5>Link: {val.link}</h5>
+              <h5>Payment Method: {val.paymentMethod}</h5>
+              <h5>Group Limit: {val.groupLimit}</h5>
+              <h5>Expiration Date: {val.expirationDate}</h5>
             </div>
           );
         })}
