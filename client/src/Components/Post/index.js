3941bcf19eca7d9f909e9b1183cff015895a4f20
Thomas Huang <yenshuo2@illinois.edu>
2022-04-28 16:48:08 -0500

    Join&Leave Post

--- client/src/Components/Post/index.js
+++ client/src/Components/Post/index.js
@@ -2,28 +2,31 @@ import React, { useState, useEffect } from "react";
 import Axios from 'axios';
 import './Post.css';
 import { useParams } from "react-router-dom";
+import useToken from '../../useToken';
 
 const PostComponent = (props) => {
   const {id} = useParams()
   console.log(useParams())
-    //Post State
+  
+  //Post State
   const [state,setState] = useState({
 
-  })
-  console.log(state)
-  useEffect(()=> {
+    })
+    console.log(state)
+    useEffect(()=> {
       if (id)
       Axios.get("http://localhost:3001/post/" + id, {
     }).then((response)=>{
       setState(response.data?.[0])
     }).catch((err)=>{
       console.error(err)
-    })
-  },[id])
+    })    
+    },[id])
 
   //Group State
   const [groupState, setGroupState] = useState([])
   console.log(groupState)
+  const { token, setToken } = useToken();
   useEffect(()=> {
       if (id)
       Axios.get("http://localhost:3001/post/group/" + id, {
@@ -32,15 +35,42 @@ const PostComponent = (props) => {
       setGroupState(response.data?response.data:[])
     }).catch((err)=>{
       console.error(err)
-    })
-  },[id])
+    })    
+    },[id])
 
-  const var_item = groupState.map((item,index)=> {
+  const var_item = groupState?.map((item,index)=> {
     return (
       <li key={index}> {item.userName} </li>
       )
+    })
+    console.log(var_item)
+
+ // Join function 
+ const join = () => {
+  console.log(token,id)
+  Axios.post("http://localhost:3001/post/join/", {userId: token.split(" ")[1], postId:id
+  }).then((response)=>{
+    console.log({response})
+    if (typeof window==="undefine") return
+        window.location= "/Components/Post/" + id 
+  }).catch((err)=>{
+    console.error(err)
   })
-  console.log(var_item)
+ }
+
+
+ //Leave Function
+  // "http://localhost:3001/post/join/?postId=" + id + '&userId=' + token.split(" ")[1]
+  const leave = () => {
+    console.log(token,id)
+    Axios.delete("http://localhost:3001/post/leave/"+ id + '/' + token.split(" ")[1]).then((response)=>{
+      console.log({response})
+      if (typeof window==="undefine") return
+          window.location= "/Components/Post/" + id 
+    }).catch((err)=>{
+      console.error(err)
+    })
+  }
 
   return (
     <div className="card3">
@@ -52,28 +82,24 @@ const PostComponent = (props) => {
           <h3>Group Limit: {state.groupLimit} </h3>
           <h3>Link: {state.link} </h3>
         </div>
-
         <div>
           <h3>Current Group Member</h3>
           <ol>
-            {groupState.length ? var_item : null }
+            {groupState?.length ? var_item : null }
           </ol>
         </div>
-
         <div>
           <h3>Average Price: </h3>
-          {/* Math.round */}
-          {/* <h5> ${(state.price/groupState.length).toFixed(2)} / person</h5> */}
+          <h5> ${(state.price/groupState?.length).toFixed(2)} / person</h5>
         </div>
-
         <div>
-          <button>Join</button>
+          <button onClick = {join} >Join Group</button>
           {/* <button>Update</button> */}
-          <button>Delete</button>
+          <button onClick = {leave}>Leave Group</button>
         </div>
-
-    </div>
-  )
+      </div>
+    )
+ 
 }
 
 
