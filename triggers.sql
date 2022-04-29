9d00bd1b625f39ce5104f4492c8153955ac3bf8c
Ken Wu <kenwu1009us@gmail.com>
2022-04-29 14:25:14 -0500

    stored propcedure & triggers query

---
+++ triggers.sql
@@ -0,0 +1,8 @@
+BEGIN
+			IF EXISTS (SELECT userName
+						FROM User
+                        WHERE userName = NEW.userName)
+				THEN SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'This username is already being used, please try another username.';
+            END IF;
+        END
