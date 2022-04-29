cadf1d72d48af1718f548bb61a61117bcfa58067
Wu, Meg <ccwu3@illinois.edu>
2022-04-29 14:57:05 -0500

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -3,12 +3,12 @@ CREATE TRIGGER UserInsertion
 	BEFORE INSERT ON User
     	FOR EACH ROW
 		BEGIN
-			IF EXISTS (
+		    IF EXISTS (
 				SELECT userName
 				FROM User
                         	WHERE userName = NEW.userName)
-				THEN SIGNAL SQLSTATE '45000'
-				SET MESSAGE_TEXT = 'This username is already being used, please try another username.';
-           		 END IF;
+			THEN SIGNAL SQLSTATE '45000'
+			SET MESSAGE_TEXT = 'This username is already being used, please try another username.';
+		    END IF;
         	END $$
 DELIMITER ;
